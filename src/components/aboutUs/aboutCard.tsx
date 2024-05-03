9b0fcba26754f0f99e4882f3e5948a18eeede44b
Daniel Santana <danielsantanachastel@gmail.com>
2024-05-03 18:44:13 -0300

    text-center

--- src/components/aboutUs/aboutCard.tsx
+++ src/components/aboutUs/aboutCard.tsx
@@ -13,7 +13,7 @@ export const AboutCard = ({body,icon,title}: AboutUsCardType)=> {
     return(
         <Card className="border-primary/40 max-w-96 mx-auto flex flex-col items-center gap-4 group hover:border-primary transition-all duration-500 overflow-hidden">
             <CardHeader>
-                <div className="text-primary text-7xl mb-4 w-full text-center">
+                <div className="text-primary text-7xl mb-4 w-full">
                     {icon}
                 </div>
                 <CardTitle>
@@ -21,7 +21,7 @@ export const AboutCard = ({body,icon,title}: AboutUsCardType)=> {
                 </CardTitle>
             </CardHeader>
             <CardContent>
-                <p className="font-light">{body}</p>
+                <p className="font-light text-center">{body}</p>
             </CardContent>
             <div className="w-0 h-1 rounded-full bg-primary group-hover:w-full transition-all duration-500"></div>
         </Card>
